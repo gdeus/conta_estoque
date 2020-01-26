5b472aae0c19bbd00d76fe9489cabd9608ea7bd6
Gabriel <dedeus.gabriel@gmail.com>
2020-01-26 14:46:40 -0300

    primeiro commit

--- lib/models/Produto.dart
+++ lib/models/Produto.dart
@@ -2,5 +2,12 @@ class Produto{
   String nome;
   int quantidadeIdeal;
   int quantidadeTotal;
-  String forcenedor;
+  String fornecedor;
+
+  Produto(nome, quantidadeIdeal, quantidadeTotal, fornecedor){
+    this.nome = nome;
+    this.quantidadeIdeal = quantidadeIdeal;
+    this.quantidadeTotal = quantidadeTotal;
+    this.fornecedor = fornecedor;
+  }
 }
--- lib/screens/Home.dart
+++ lib/screens/Home.dart
@@ -1,3 +1,4 @@
+import 'package:conta_estoque/models/Produto.dart';
 import 'package:conta_estoque/screens/Pedido.dart';
 import 'package:flutter/material.dart';
 
@@ -7,6 +8,9 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+
+  List<Produto> listProdutoss = [];
+  int quantidadeProdutos;
 
   String dropdownValue = 'Ambev';
   @override
@@ -22,13 +26,13 @@ class _HomeScreenState extends State<HomeScreen> {
             width: MediaQuery.of(context).size.width * 0.95,
               child: DropdownButton<String>(
                   value: dropdownValue,
-                  itemHeight: 40.0,
                   icon: Icon(Icons.arrow_downward),
                   iconSize: 30,
                   elevation: 14,
                   isExpanded: true,
                   style: TextStyle(
-                      color: Colors.deepOrange,
+                    color: Colors.deepOrange,
+                    fontWeight: FontWeight.bold
                   ),
                   underline: Container(
                       height: 2,
@@ -45,90 +49,41 @@ class _HomeScreenState extends State<HomeScreen> {
                       value: value,
                       child: Text(value),
                     );
-                  })
-                      .toList(),
+                  }).toList(),
                 ),
             ),
-          Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Expanded(
-                flex: 3,
-                child: Text("Corona",
-                style: TextStyle(
-                  fontSize: 30,
-                  fontWeight: FontWeight.bold
-                ),
-                ),
+          Expanded(
+            child: Container(
+              child: FutureBuilder(
+                future: criaProdutos(),
+                builder: (BuildContext context, AsyncSnapshot snapshot){
+                  switch(snapshot.connectionState){
+                    case ConnectionState.none:
+                    case ConnectionState.waiting:
+                      return Container(
+                        child: Center(
+                          child: CircularProgressIndicator(),
+                        ),
+                      );
+                    default:
+                      return Column(
+                        children: <Widget>[
+                          Expanded(
+                            child: ListView.builder(
+                              padding: EdgeInsets.only(top: 10.0),
+                              itemCount: quantidadeProdutos,
+                              itemBuilder: (context, index){
+                                print("Cheguei aqui");
+                                return ProdutoRow(listProdutoss[index]);
+                              },
+                            ),
+                          )
+                        ],
+                     );
+                   }
+                },
               ),
-              Expanded(
-                flex: 1,
-                child: TextField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(borderRadius:  BorderRadius.only())
-                  ),
-
-                ),
-              )
-
-            ],
-          ),
-          Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Expanded(
-                flex: 3,
-                child: Text("Budwaiser",
-                  style: TextStyle(
-                      fontSize: 20
-                  ),),
-              ),
-              Expanded(
-                flex: 1,
-                child: TextField(
-
-                ),
-              )
-
-            ],
-          ),
-          Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Expanded(
-                flex: 3,
-                child: Text("Guaraná 2L",
-                  style: TextStyle(
-                      fontSize: 20
-                  ),),
-              ),
-              Expanded(
-                flex: 1,
-                child: TextField(
-
-                ),
-              )
-
-            ],
-          ),
-          Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Expanded(
-                flex: 3,
-                child: Text("Guaraná Lata",
-                  style: TextStyle(
-                      fontSize: 20
-                  ),),
-              ),
-              Expanded(
-                flex: 1,
-                child: TextField(
-
-                ),
-              )
-
-            ],
+            ),
           ),
           RaisedButton(
             onPressed: () {
@@ -146,7 +101,43 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 
-  void criaProdutos(){
+  criaProdutos() async{
+    List<Produto> listProdutos = [
+      new Produto('Skol', 85, 0, 'Ambev'),
+      new Produto('Budwaiser', 45, 0, 'Ambev'),
+      new Produto('Batata Frisé', 30, 0, 'Polina'),
+      new Produto('Batata Rústica', 42, 0, 'Ambev')
+    ];
 
+    listProdutoss = listProdutos;
+    quantidadeProdutos = listProdutoss.length;
+
+  }
+
+  Widget ProdutoRow(Produto produto){
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: <Widget>[
+        Expanded(
+          flex: 3,
+          child: Text(produto.nome,
+            style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold
+            ),
+          ),
+        ),
+        Expanded(
+          flex: 1,
+          child: TextField(
+            decoration: InputDecoration(
+                border: OutlineInputBorder(borderRadius:  BorderRadius.only())
+            ),
+
+          ),
+        )
+
+      ],
+    );
   }
 }
